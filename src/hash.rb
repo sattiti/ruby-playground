cfa8bc69739990da3fc37b4101a363019efd1503
sattiti <sattiti@users.noreply.github.com>
2019-09-05 16:26:06 +0900

    update

--- src/hash.rb
+++ src/hash.rb
@@ -2,8 +2,16 @@
 
 require 'pp'
 
-# 多次元 hash の初期化
+# 多次元 hash の初期化 {{{
 data = Hash.new {|h, k| h[k] = Hash.new(&h.default_proc)}
 data[:a][:b][:c] = 'hello world'
 
 pp data
+#}}}
+
+# 多重動的ハッシュ生成 {{{
+path      = ['about', 'me', 'index']
+nest_hash = Hash.new{|h, k| h[k] = Hash.new(&h.default_proc)}
+path.inject(nest_hash, :[])
+pp nest_hash
+# }}}
